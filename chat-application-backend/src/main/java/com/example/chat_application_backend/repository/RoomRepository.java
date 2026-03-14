06ab2c83ee5e4130d9da4dfc132d89489a291b00
gururaj02 <ngururaj58@gmail.com>
2026-03-14 19:48:42 +0530

    Rooms created

--- chat-application-backend/src/main/java/com/example/chat_application_backend/repository/RoomRepository.java
+++ chat-application-backend/src/main/java/com/example/chat_application_backend/repository/RoomRepository.java
@@ -1,4 +1,9 @@
 package com.example.chat_application_backend.repository;
 
-public interface RoomRepository {
+import com.example.chat_application_backend.entities.Room;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RoomRepository extends MongoRepository<Room, String> {
+    //get room using room id
+    Room findByRoomId(String roomId);
 }
